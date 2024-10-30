9d6c6787fd51af408649140b7d92d2c11df91add
Igor Kirpinev <kirpinevigor@yandex.ru>
2024-10-30 16:09:34 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,155 @@
+import { ButtonMobile } from "@alfalab/core-components/button/mobile";
+
+import { Typography } from "@alfalab/core-components/typography";
+import { useState } from "react";
+import smart from "./assets/smart.png";
+import smile from "./assets/smile.png";
+import drums from "./assets/drums.png";
+import smileArrow from "./assets/smile-arrow.png";
+import gift from "./assets/gift.png";
+import percent from "./assets/percent.png";
+import free from "./assets/free.png";
+import { LS, LSKeys } from "./ls";
+import { appSt } from "./style.css";
+import { ThxLayout } from "./thx/ThxLayout";
+import { Gap } from "@alfalab/core-components/gap";
+
+interface Product {
+  title: string;
+  text: string;
+  image: string;
+}
+
+const products: Array<Product> = [
+  {
+    title: "+1 топовая категория кэшбэка",
+    text: "5% на самое популярное",
+    image: smileArrow,
+  },
+  {
+    title: "+1 попытка крутить барабан суперкэшбэка",
+    text: "Выше шанс выиграть до 100% в случайной категории",
+    image: drums,
+  },
+  {
+    title: "Секретная подборка партнёров с кэшбэком",
+    text: "Доступ к специальным предложениям",
+    image: gift,
+  },
+  {
+    title: "+1% годовых",
+    text: "По накопительному Альфа-Счёту на ежедневный остаток",
+    image: percent,
+  },
+  {
+    title: "Бесплатные уведомления",
+    text: "Пуши и смс об операциях по всем дебетовым картам",
+    image: free,
+  },
+];
+
+export const App = () => {
+  const [thxShow, setThx] = useState(LS.getItem(LSKeys.ShowThx, false));
+
+  const clickSubmit = () => {
+    // window.gtag("event", "connect_click", {
+    //   variant_name: "ghk_3579_android_14_17_4",
+    // });
+  };
+
+  const submit = () => {
+    clickSubmit();
+    LS.setItem(LSKeys.ShowThx, true);
+    setThx(true);
+  };
+
+  if (thxShow) {
+    return <ThxLayout />;
+  }
+
+  return (
+    <>
+      <div className={appSt.container}>
+        <div className={appSt.box}>
+          <img src={smart} alt="Картинка Альфа-Смарт" />
+          <Typography.TitleResponsive
+            tag="h1"
+            view="medium"
+            font="system"
+            weight="bold"
+          >
+            Альфа-Смарт
+          </Typography.TitleResponsive>
+          <Typography.Text view="primary-medium" color="secondary">
+            Стоимость — 299 руб/мес
+          </Typography.Text>
+        </div>
+
+        <div className={appSt.subscription}>
+          <img src={smile} alt="" width={24} height={24} />
+          <Typography.Text
+            view="primary-medium"
+            className={appSt.subscriptionText}
+          >
+            Подписка стоит 299 ₽, если тратите с карты 20 000 ₽ в месяц. Если
+            тратите меньше — 399 ₽
+          </Typography.Text>
+        </div>
+
+        <Gap size={8} />
+
+        <div className={appSt.products}>
+          <Typography.TitleResponsive
+            font="system"
+            tag="h2"
+            weight="bold"
+            view="small"
+            className={appSt.productsTitle}
+          >
+            В вашей подписке
+          </Typography.TitleResponsive>
+
+          {products.map((product) => (
+            <div className={appSt.product} key={product.title}>
+              <div>
+                <Typography.TitleResponsive
+                  font="system"
+                  view="small"
+                  weight="bold"
+                  tag="h3"
+                  className={appSt.productTitle}
+                >
+                  {product.title}
+                </Typography.TitleResponsive>
+
+                <Typography.Text
+                  view="secondary-large"
+                  tag="p"
+                  color="secondary"
+                  className={appSt.productText}
+                >
+                  {product.text}
+                </Typography.Text>
+              </div>
+              <img
+                src={product.image}
+                alt=""
+                width={96}
+                height={96}
+                className={appSt.productIcon}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <Gap size={72} />
+
+      <div className={appSt.bottomBtn}>
+        <ButtonMobile block view="primary" onClick={submit}>
+          Подключить
+        </ButtonMobile>
+      </div>
+    </>
+  );
+};
